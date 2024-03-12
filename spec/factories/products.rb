400d377fda29f9c009354e6c3879843596a6ab0b
Alvaro Alday <xxchronos35xx@gmail.com>
2024-03-12 17:36:54 -0600

    Update factories

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :product do
-    
+    name { Faker::Name.name }
+    description { Faker::Lorem.sentence }
+    price_cents { SecureRandom.random_number(10000) }
+    age_low_weeks { 0 }
+    age_high_weeks { 12 }
   end
 end
