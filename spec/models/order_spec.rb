3aec8bfea02251509f3b49725d6cbe86a7da7048
Alvaro Alday <xxchronos35xx@gmail.com>
2024-03-12 11:08:33 -0600

    Uncomment model specs for order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,25 +1,25 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Order, type: :model do
-#   describe "#validations" do
-#     context 'with invalid attributes' do
-#       subject(:order) { build :order, shipping_name: nil }
+RSpec.describe Order, type: :model do
+  describe "#validations" do
+    context 'with invalid attributes' do
+      subject(:order) { build :order, shipping_name: nil }
 
-#       before { is_expected.to be_invalid }
+      before { is_expected.to be_invalid }
 
-#       it "has shipping_name can't be blank error" do
-#         expect(order.errors[:shipping_name]).to include "can't be blank"
-#       end
-#     end
+      it "has shipping_name can't be blank error" do
+        expect(order.errors[:shipping_name]).to include "can't be blank"
+      end
+    end
 
-#     context 'with blank attributes' do
-#       subject(:order) { described_class.new() }
+    context 'with blank attributes' do
+      subject(:order) { described_class.new() }
 
-#       before { is_expected.to be_invalid }
+      before { is_expected.to be_invalid }
 
-#       it "has shipping_name can't be blank error" do
-#         expect(order.errors[:shipping_name]).to include "can't be blank"
-#       end
-#     end
-#   end
-# end
+      it "has shipping_name can't be blank error" do
+        expect(order.errors[:shipping_name]).to include "can't be blank"
+      end
+    end
+  end
+end
