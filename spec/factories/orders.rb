e7344d1e0db9e6e89c2aa039ff1a12ba68a5c915
Alvaro Alday <xxchronos35xx@gmail.com>
2024-03-12 11:02:03 -0600

    Introduce factories to project

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order do
     association(:product)
+    shipping_name { Faker::Name.name }
+    address { Faker::Address.street_address }
+    zipcode { Faker::Address.zip }
+    user_facing_id { SecureRandom.random_number(1_000_000) }
   end
 end
