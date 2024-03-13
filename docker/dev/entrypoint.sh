66525ba0b131cd31077d8e8f10c69d749f36c60e
Alvaro Alday <xxchronos35xx@gmail.com>
2024-03-13 11:56:29 -0600

    Remove unused logic from Docker environment

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -13,19 +13,6 @@ rails assets:precompile
 # Migrate database
 rails db:create db:migrate db:seed
 
-# Add minio alias
-mc alias set minio $S3_ENDPOINT $S3_ACCESS_KEY $S3_SECRET_KEY
-
-# Check if the bucket exists
-if mc ls minio | grep -q "$S3_BUCKET"; then
-  echo "Bucket already exists"
-else
-  # Create the bucket
-  mc mb "minio/$S3_BUCKET"
-  mc anonymous set download "minio/$S3_BUCKET"
-  echo "Bucket created successfully"
-fi
-
 # Remove servier .pid file
 rm tmp/pids/server.pid
 
